32dcfed3dc121bc64ac866efee2e85202c6be905
RiRi <riyan.pahuja4@gmail.com>
2024-09-12 18:23:58 +1000

    Initial Commit

---
+++ LandmarkMapApp/LandmarkMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkMapAppApp.swift
+//  LandmarkMapApp
+//
+//  Created by Riyan Pahuja on 12/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
